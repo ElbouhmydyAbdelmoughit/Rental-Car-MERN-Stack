1f745d799d187ae86bcdde8992158bcdd1222096
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-03-21 22:16:01 +0000

    finished api auth

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use("/auth", authRouter);
+app.use("/", authRouter);
 // app.use("/organizme", organizmeRouter);
 // app.use("/formation", formationRouter);
 app.use(ErrorHandler);
