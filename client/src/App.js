9f9a6b3b50627ec09167b150d7d8f05c359fe8cc
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-03-28 21:29:05 +0000

    create header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
-import {BrowserRouter, Router, Route} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 
+import Home from "./pages/Home";
+// import Login from "./components/Login/Login";
+// import Register from "./components/Register/Register";
+// import Reset from "./components/Reset/Reset";
+// import Client from "./components/Client/Client";
+// import Livreur from "./components/Livreur/Livreur";
+import ERROR404 from "./pages/ERROR404/ERROR404";
 
-<BrowserRouter>
-  <Router>
-    
-  </Router>
-</BrowserRouter>
+const App = () => {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<ERROR404 />} />
+          <Route path="/" element={<Home />} />
+          {/* <Route path="Login" element={<Login />} />
+          <Route path="Register" element={<Register />} />
+          <Route path="Reset" element={<Reset />} />
+          <Route path="Client" element={<Client />} />
+          <Route path="Livreur" element={<Livreur />} />  */}
+        </Routes>
+      </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg" className='shadow'>
+      <Container fluid>
+        <Navbar.Brand href="" className='fw-bold'>CAR RENTAL</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">AboutUs</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
