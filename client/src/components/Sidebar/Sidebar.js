997f9acd045a474191ff1da0b03fad5c92f481ae
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-04-05 14:48:10 +0000

    finished design sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,188 +1,71 @@
 import React from "react";
 import "./Sidebar.css";
+import "tippy.js/animations/scale.css";
+import { NavLink, useNavigate } from "react-router-dom";
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
   return (
     <>
       <div className="container-fluid">
         <div className="row flex-nowrap">
+          <input type="checkbox" id="menu" className="d-none" />
           <div
-            className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark"
-            style={{ minWidth: "200px" }}
+            style={{ background: "#000" }}
+            id="sid"
+            className="col-auto col-xl-2 min-vh-100 text-center"
           >
-            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-              <a
-                href="/"
-                className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+            <nav className="text-sm-start mt-5 pb-5 p-sm-2">
+              <NavLink
+                className="nav-link p-1 p-sm-2 text-white"
+                to={"/statistique"}
               >
-                <span className="fs-5 d-none d-sm-inline">Menu</span>
-              </a>
-              <ul
-                className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                id="menu"
+                <i className="bi bi-speedometer2 fs-5 me-sm-2"></i>
+                <span className="d-none d-sm-inline">Dashboard</span>
+              </NavLink>
+              <br />
+              <NavLink
+                className="nav-link p-1 p-sm-2 text-white"
+                to={"/voyage"}
               >
-                <li className="nav-item">
-                  <a href="#" className="nav-link align-middle px-0">
-                    <i className="fs-4 bi-house"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Home</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#submenu1"
-                    data-bs-toggle="collapse"
-                    className="nav-link px-0 align-middle"
-                  >
-                    <i className="fs-4 bi-speedometer2"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Dashboard</span>{" "}
-                  </a>
-                  <ul
-                    className="collapse show nav flex-column ms-1"
-                    id="submenu1"
-                    data-bs-parent="#menu"
-                  >
-                    <li className="w-100">
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Item</span> 1{" "}
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Item</span> 2{" "}
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#" className="nav-link px-0 align-middle">
-                    <i className="fs-4 bi-table"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Orders</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#submenu2"
-                    data-bs-toggle="collapse"
-                    className="nav-link px-0 align-middle "
-                  >
-                    <i className="fs-4 bi-bootstrap"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Bootstrap</span>
-                  </a>
-                  <ul
-                    className="collapse nav flex-column ms-1"
-                    id="submenu2"
-                    data-bs-parent="#menu"
-                  >
-                    <li className="w-100">
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Item</span> 1
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Item</span> 2
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a
-                    href="#submenu3"
-                    data-bs-toggle="collapse"
-                    className="nav-link px-0 align-middle"
-                  >
-                    <i className="fs-4 bi-grid"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Products</span>{" "}
-                  </a>
-                  <ul
-                    className="collapse nav flex-column ms-1"
-                    id="submenu3"
-                    data-bs-parent="#menu"
-                  >
-                    <li className="w-100">
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Product</span> 1
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Product</span> 2
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Product</span> 3
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Product</span> 4
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#" className="nav-link px-0 align-middle">
-                    <i className="fs-4 bi-people"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Customers</span>{" "}
-                  </a>
-                </li>
-              </ul>
-              <hr />
-              <div className="dropdown pb-4">
-                <a
-                  href="#"
-                  className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                  id="dropdownUser1"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img
-                    src="https://github.com/mdo.png"
-                    alt="hugenerd"
-                    width="30"
-                    height="30"
-                    className="rounded-circle"
-                  />
-                  <span className="d-none d-sm-inline mx-1">loser</span>
-                </a>
-                <ul
-                  className="dropdown-menu dropdown-menu-dark text-small shadow"
-                  aria-labelledby="dropdownUser1"
-                >
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      New project...
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Profile
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Sign out
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+                <i className="bi bi-grid fs-5 me-sm-2"></i>
+                <span className="d-none d-sm-inline">voyage</span>
+              </NavLink>
+              <br />
+              <NavLink className="nav-link p-1 p-sm-2 text-white" to={"/agent"}>
+                <i className="bi bi-person-vcard fs-5 me-sm-2"></i>
+                <span className="d-none d-sm-inline">Agent</span>
+              </NavLink>
+              <br />
+              <NavLink
+                className="nav-link p-1 p-sm-2 text-white"
+                to={"/client"}
+              >
+                <i className="bi bi-people me-sm-2 fs-5"></i>
+                <span className="d-none d-sm-inline">Client</span>
+              </NavLink>
+              <br />
+              <NavLink
+                className="nav-link p-1 p-sm-2 text-white"
+                to={"/profile"}
+              >
+                <i className="bi bi-person me-sm-2 fs-5"></i>
+                <span className="d-none d-sm-inline">Profil</span>
+              </NavLink>
+              <br />
+              <NavLink className="nav-link p-1 p-sm-2 text-white" to={"/#"}>
+                <i className="bi bi-sliders2 me-sm-2 fs-5"></i>
+                <span className="d-none d-sm-inline">Settings</span>
+              </NavLink>
+              <br />
+              <button className="nav-link p-1 p-sm-2 text-white" to={"/#"}>
+                <i className="bi bi-box-arrow-right me-sm-2 fs-5"></i>
+                <span className="d-none d-sm-inline">Logout</span>
+              </button>
+            </nav>
           </div>
           <div className="col py-3">
             <div className="container mt-5">
